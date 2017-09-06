071d493a3e58c969c18bed6740e136508f398f69
Administrador <Administrador@Formacion-TOSH7.ipartekaula.com>
2017-09-06 09:18:54 +0200

    calcular vueltas actualizado

--- src/CalcularVueltas.java
+++ src/CalcularVueltas.java
@@ -25,82 +25,37 @@
  * */
 public class CalcularVueltas {
 	
-	final double [] BILLETES_MONEDAS= {50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01};
+	final static float [] BILLETES_MONEDAS= {50f,20f,10f,5f,2f,1f,0.5f,0.2f,0.1f,0.05f,0.02f,0.01f};
+	static float compraTotal=1326.24f;
+	static float vueltas=673.76f;
+	
+	static int []numDineroUsado= {0,0,0,0,0,0,0,0,0,0,0,0};
 	
 	public static void main(String[] args) {
 		
-		double compraTotal=1326.24;
-		double gasto= 4*500;
-		double [] billetes = {500,50,20,10,5};
-		double [] monedas = {2,1,0.5,0.2,0.1,0.05,0.02,0.01};
-		double vueltas=gasto-compraTotal;
 		
-		int []numBilletesUsados= {0,0,0,0,0};
-		int []numMonedasUsados= {0,0,0,0,0,0,0,0};
 		
-		while(vueltas-billetes[1]>0) {
+		for (int i = 0; i < BILLETES_MONEDAS.length; i++) {
 			
-			vueltas=vueltas-billetes[1];
-			numBilletesUsados[1]++;
-		}while(vueltas-billetes[2]>0) {
-			
-			vueltas=vueltas-billetes[2];
-			numBilletesUsados[2]++;
-		}while(vueltas-billetes[3]>0) {
-			
-			vueltas=vueltas-billetes[3];
-			numBilletesUsados[3]++;
-		}while(vueltas-billetes[4]>0) {
-			
-			vueltas=vueltas-billetes[4];
-			numBilletesUsados[4]++;
-		}while(vueltas-monedas[0]>0) {
-			
-			vueltas=vueltas-monedas[0];
-			numMonedasUsados[0]++;
-		}while(vueltas-monedas[1]>0) {
-			
-			vueltas=vueltas-monedas[1];
-			numMonedasUsados[1]++;
-		}while(vueltas-monedas[2]>0) {
-			
-			vueltas=vueltas-monedas[2];
-			numMonedasUsados[2]++;
-		}while(vueltas-monedas[3]>0) {
-			
-			vueltas=vueltas-monedas[3];
-			numMonedasUsados[3]++;
-		}while(vueltas-monedas[4]>0) {
-			
-			vueltas=vueltas-monedas[4];
-			numMonedasUsados[4]++;
-		}while(vueltas-monedas[5]>0) {
-			
-			vueltas=vueltas-monedas[5];
-			numMonedasUsados[5]++;
-		}while(vueltas-monedas[6]>0) {
-			
-			vueltas=vueltas-monedas[6];
-			numMonedasUsados[6]++;
-		}while(vueltas-monedas[7]>0) {
-			
-			vueltas=vueltas-monedas[7];
-			numMonedasUsados[7]++;
+			while(vueltas-BILLETES_MONEDAS[i]>=0) {
+				
+				vueltas=vueltas-BILLETES_MONEDAS[i];
+				numDineroUsado[i]++;
+			}
 		}
 		
-		
-		System.out.println("Billetes de 50 " +numBilletesUsados[1]);
-		System.out.println("Billetes de 20 " +numBilletesUsados[2]);
-		System.out.println("Billetes de 10 " +numBilletesUsados[3]);
-		System.out.println("Billetes de 5 " +numBilletesUsados[4]);
-		System.out.println("Monedas de 2 "+numMonedasUsados[0]);
-		System.out.println("Monedas de 1 "+numMonedasUsados[1]);
-		System.out.println("Monedas de 0.50 "+numMonedasUsados[2]);
-		System.out.println("Monedas de 0.20 "+numMonedasUsados[3]);
-		System.out.println("Monedas de 0.10 "+numMonedasUsados[4]);
-		System.out.println("Monedas de 0.5 "+numMonedasUsados[5]);
-		System.out.println("Monedas de 0.2 "+numMonedasUsados[6]);
-		System.out.println("Monedas de 0.1 "+numMonedasUsados[7]);
+		System.out.println("Billetes de 50: " +numDineroUsado[0]);
+		System.out.println("Billetes de 20: " +numDineroUsado[1]);
+		System.out.println("Billetes de 10: " +numDineroUsado[2]);
+		System.out.println("Billetes de 5: " +numDineroUsado[3]);
+		System.out.println("Monedas de 2: "+numDineroUsado[4]);
+		System.out.println("Monedas de 1: "+numDineroUsado[5]);
+		System.out.println("Monedas de 0.50: "+numDineroUsado[6]);
+		System.out.println("Monedas de 0.20: "+numDineroUsado[7]);
+		System.out.println("Monedas de 0.10: "+numDineroUsado[8]);
+		System.out.println("Monedas de 0.5: "+numDineroUsado[9]);
+		System.out.println("Monedas de 0.02: "+numDineroUsado[10]);
+		System.out.println("Monedas de 0.01: "+numDineroUsado[11]);
 		
 	}
 	
