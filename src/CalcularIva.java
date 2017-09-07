0950c7ce99fe9232b5b2ff05226796b6843c6b71
Administrador <Administrador@Formacion-TOSH7.ipartekaula.com>
2017-09-07 09:04:14 +0200

    calculariva actualizado

--- src/CalcularIva.java
+++ src/CalcularIva.java
@@ -4,21 +4,46 @@ public class CalcularIva {
 
 	public static void main(String[] args) {
 
-		System.out.println("Calculadora IVA 21%");
-		System.out.println("-------------------");
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("\t\t\t\tCalculadora IVA 21%");
+		System.out.println("\t\t\t-----------------------------------");
 
 		System.out.println("Por favor dame el importe en €");
 
-		Scanner sc = new Scanner(System.in);
 		float importe = sc.nextFloat();
-		String respuesta=sc.nextLine();
 
 		System.out.println("iva repercutido: " + importe * 0.21);
+		
+		System.out.println();
+		
+		System.out.println("¿Quieres volver a teclear el importe?");
 
-		while (respuesta.equals("s")) {
+		String respuesta = sc.next();
+
+		System.out.println(respuesta);
+
+		while ("s".equalsIgnoreCase(respuesta)) {
+
+			System.out.println();
+			
+			System.out.println("Por favor dame el importe en €");
+			
+			importe = sc.nextFloat();
+
+			System.out.println();
+
+			System.out.println("iva repercutido: " + importe * 0.21);
+			
+			System.out.println();
 
 			System.out.println("¿Quieres volver a teclear el importe?");
+			
+			respuesta = sc.next();
 		}
+		System.out.println();
+
+		System.out.println("Gracias por venir");
 
 	}
 }
