a920d67e92b1161a6652a86e899a5f0788e4df97
Administrador <Administrador@Formacion-TOSH7.ipartekaula.com>
2017-09-07 12:26:08 +0200

    tocaleer actualizado

--- src/TocaLeer.java
+++ src/TocaLeer.java
@@ -5,26 +5,62 @@ public class TocaLeer {
 	 * @param args
 	 */
 	
-	static String [] alumnos= {"Arkaitz",
-								"Erlantz",
-								"Fran",
-								"Cristian",
-								"Leire",
-								"Mikel",
-								"Josu",
-								"Alberto",
-								"Jon",
-								"Naiara",
-								"Lander",
-								"German"};
+	/*static String [] alumnos= {"Arkaitz",
+				            "Erlantz",
+				            "Fran",
+				            "Cristian",
+				            "Leire" ,
+				            "Mikel",
+				            "Josu",
+				            "Alberto",
+				            "Jon",
+				            "Arantza",
+				            "Naiara",
+				            "Lander",
+				            "Yeray",
+				            "German"};*/
+	
+	 
+	 
+	 
+	 
+	 
+		
+	 
+	
+	 
+	
+
+
+	 
+	 
+
+	static Participante[]alumnos = new Participante[15];
+	
 	
 	public static void main(String[] args) {
+		
+		alumnos[0]= new Participante("Arkaitz", "arkaitzgl");
+		alumnos[1]= new Participante("Erlantz1", "erliglesias");
+		alumnos[2]= new Participante("Fran", "FranPerez92");
+		alumnos[3]= new Participante("Cristhian", "CristianVar");
+		alumnos[4]= new Participante("Erlantz2", "-Erlantzcabrejas ");
+		alumnos[5]= new Participante("Leire", "LeireBustin");
+		alumnos[6]= new Participante("mikel", "zoroa84");
+		alumnos[7]= new Participante("Josu Larrazabal", "Katakume");
+		alumnos[8]= new Participante("Alberto", "amsoria91");
+		alumnos[9]= new Participante("Jon alarcon", "alarconsolis");
+		alumnos[10]= new Participante("", "");
+		alumnos[11]= new Participante("Naiara Mateos", "naiaramateos");
+		alumnos[12]= new Participante("Lander", "LanderAB");
+		alumnos[13]= new Participante("Yeray", "LordBlacKhiin");
+		alumnos[14]= new Participante("", "");
 		
 		int numAleatorio = generarNumeroAleatorio(alumnos.length);
 		
 		//generar numero aleatorio
 		
-		System.out.println("El afortunad@ es: "+alumnos[numAleatorio]);
+		System.out.println("El afortunad@ es: "+alumnos[numAleatorio].getNombre());
 		
 		
 	}
