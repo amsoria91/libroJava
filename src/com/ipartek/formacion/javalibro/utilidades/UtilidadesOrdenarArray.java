bd9b3e38b20baeead20e7915a02ddb6a25a6423d
Administrador <Administrador@Formacion-TOSH7.ipartekaula.com>
2017-09-11 13:55:59 +0200

    actualizado

--- src/com/ipartek/formacion/javalibro/utilidades/UtilidadesOrdenarArray.java
+++ src/com/ipartek/formacion/javalibro/utilidades/UtilidadesOrdenarArray.java
@@ -15,7 +15,7 @@ public class UtilidadesOrdenarArray {
 		int[] resul = aDesordenado;
 		int numeroApoyo = 0;
 		
-		if(orden=true) {
+		if(orden=false) {
 			for (int i = 0; i < resul.length; i++) {
 
 				for (int j = 0; j < resul.length-1; j++) {
--- src/com/ipartek/formacion/javalibro/utilidades/UtilidadesOrdenarArrayTest.java
+++ src/com/ipartek/formacion/javalibro/utilidades/UtilidadesOrdenarArrayTest.java
@@ -11,13 +11,13 @@ public class UtilidadesOrdenarArrayTest extends TestCase {
 		
 		int[] aDesordando = {0,3,1,8,7,2,5,4,6,9};
 		
-		int[] ordenadorMenorMayor = UtilidadesOrdenarArray.ordenarArray(aDesordando, true);
+		int[] ordenadorMenorMayor = UtilidadesOrdenarArray.ordenarArray(aDesordando, false);
 		
 		for (int i = 0; i < ordenadorMenorMayor.length; i++) {
 			assertEquals(i, ordenadorMenorMayor[i]);
 		}
 		
-		int[] ordenadorMayorMenor = UtilidadesOrdenarArray.ordenarArray(aDesordando, false);
+		int[] ordenadorMayorMenor = UtilidadesOrdenarArray.ordenarArray(aDesordando, true);
 		
 		for (int i = ordenadorMayorMenor.length-1; i >0; i--) {
 			assertEquals(i, ordenadorMayorMenor[i]);
