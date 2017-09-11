31e0000483d9c02fab9ea1cfe33fae3cba8bbcf5
Administrador <Administrador@Formacion-TOSH7.ipartekaula.com>
2017-09-11 13:57:27 +0200

    ordenar array finalizado

--- src/com/ipartek/formacion/javalibro/utilidades/UtilidadesOrdenarArray.java
+++ src/com/ipartek/formacion/javalibro/utilidades/UtilidadesOrdenarArray.java
@@ -7,7 +7,7 @@ public class UtilidadesOrdenarArray {
 	 * Ordena un Array con el metodo bubble-sort
 	 * @see https://www.youtube.com/watch?v=lyZQPjUT5B4
 	 * @param aDesordenado Array desordenado
-	 * @param orden true de mayor a menor, false de menor a mayor
+	 * @param orden true de menor a mayor, false de mayor a menor
 	 * @return array ordenado segun parametro <code>orden</code>
 	 */
 	static int[] ordenarArray(int[] aDesordenado, boolean orden) {
