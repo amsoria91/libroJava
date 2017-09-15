8bbf2e64b91b63af91477dbd4645579add9480ac
Administrador <Administrador@Formacion-TOSH7.ipartekaula.com>
2017-09-15 09:10:34 +0200

    terminado ListarPersonas

--- src/com/ipartek/formacion/javalibro/ficheros/ListarPersonas.java
+++ src/com/ipartek/formacion/javalibro/ficheros/ListarPersonas.java
@@ -1,5 +1,11 @@
 package com.ipartek.formacion.javalibro.ficheros;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Leer linea a linea el fichero "data/personas.txt" y mostrar por pantalla.<br>
  * Contar el total de personas leidas.
@@ -10,9 +16,51 @@ package com.ipartek.formacion.javalibro.ficheros;
  */
 public class ListarPersonas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int contPers = 0;
+	static FileReader fr = null;
+	static BufferedReader br = null;
 
-	}
+	public static void main(String[] args) {
+
+		try {
+
+			fr = new FileReader("C:\\desarrolloNuevo\\personas.txt");
+			br = new BufferedReader(fr);
+			String linea="";
+			
+			try {
+				while((linea=br.readLine())!=null) {
+					
+					System.out.println(linea);
+					contPers++;
+					
+					
+					
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("------------------------------------");
+			System.out.println(contPers+" linea leidas");
+			
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				br.close();
+				fr.close();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}//fin main
 
 }
